f91228cfe62980a06d2bba0d3c6773b6ae10b732
NoraTiku <jtiku@madisoncollege.edu>
2022-02-23 12:07:22 -0600

    using the most recent ui-react component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 
 import React from 'react';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react/legacy';
+import { Authenticator, Button } from '@aws-amplify/ui-react';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello from AWS Amplify</h1>
-      <AmplifySignOut />
-    </div>
+    <Authenticator 
+      variation='modal'
+    
+    >
+      {({signOut,user}) => (
+        <>
+        <h1>hello {user.username}</h1>
+        <button 
+       
+        onClick={signOut}>
+          Sign Out
+        </button>
+
+        </>
+      )}
+
+    </Authenticator>
+    
   );
 };
 
-export default withAuthenticator(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -6,12 +6,20 @@ import reportWebVitals from './reportWebVitals';
 
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
+import { AmplifyProvider }  from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 Amplify.configure(config);
+
+
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <AmplifyProvider>
     <App />
+    </AmplifyProvider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
